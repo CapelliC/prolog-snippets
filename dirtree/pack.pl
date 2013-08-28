c79fb27642e2eda06650b15961587d2d8a552ba4
CapelliC <cc.carlo.cap@gmail.com>
2013-08-28 20:04:58 +0200

    added dirtree pack

--- dirtree/pack.pl
+++
@@ -1,8 +0,0 @@
-name(dirtree).
-title('directory XML structure').
-version('1.0.0').
-author('Capelli Carlo', 'cc.carlo.cap@gmail.com').
-packager('Capelli Carlo', 'cc.carlo.cap@gmail.com').
-provides('XML of directory').
-requires(lambda).
-
