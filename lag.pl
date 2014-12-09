7c04b7ecb0b37330f6fd514ae523e2cbde2eeadf
CapelliC <cc.carlo.cap@gmail.com>
2014-12-09 12:05:56 +0100

    after http://stackoverflow.com/questions/27317069/collect-all-minimum-solutions-from-a-backtrackable-predicate

--- lag.pl
+++ lag.pl
@@ -1,7 +1,7 @@
 /*  File         : lag.pl
     Author       : Carlo Capelli
     E-mail       : cc.carlo.cap@gmail.com
-    Copyright (C): 2013, Carlo Capelli
+    Copyright (C): 2013,2014 Carlo Capelli
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -18,54 +18,48 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-:- module(lag,
-	  [integrate/3
-	  ]).
+:- module(lag, [integrate/3]).
 
-:- [library(aggregate)].
+:- use_module(library(apply)).
 
 integrate(min, Goal, R) :-
-	State = (_, _),
-	repeat,
-	(   call(Goal, V),
-	    arg(1, State, C),
-	    ( ( var(C) ; V @< C ) -> U = V ; U = C ),
-	    nb_setarg(1, State, U),
-	    fail
-	;   arg(1, State, R)
-	),
-	!.
+	State = state(_, _),
+	forall(call(Goal, V),
+	(	arg(1, State, C),
+		( ( var(C) ; V @< C ) -> nb_setarg(1, State, V) ; true )
+	)),
+	arg(1, State, R).
 
 integrate(min_list_associated, Goal, Min-Ws) :-
-	State = (_, [], _),
+	State = state(_, [], _),
 	forall(call(Goal, V, W),	% W stands for witness
 	(	arg(1, State, C),	% C is current min
 		arg(2, State, CW),	% CW are current min witnesses
-		( ( var(C) ; V @< C ) -> U = V, Ws = [W] ; U = C, (C == V -> Ws = [W|CW] ; Ws = CW) ),
-		nb_setarg(1, State, U),
-		nb_setarg(2, State, Ws)
+		(	( var(C) ; V @< C )
+		->	nb_setarg(1, State, V),
+			nb_setarg(2, State, [W])
+		;	C == V
+		->	nb_setarg(2, State, [W|CW])
+		;	true
+		)
 	)),
 	arg(1, State, Min), arg(2, State, Ws).
 
 integrate(count, Goal, R) :-
-	State = (0, _),
-	repeat,
-	(   call(Goal),
-	    arg(1, State, C),
-	    U is C+1,
-	    nb_setarg(1, State, U),
-	    fail
-	;   arg(1, State, R)
-	).
+	State = state(0, _),
+	forall(call(Goal),
+	(	arg(1, State, C),
+		U is C+1,
+		nb_setarg(1, State, U)
+	)),
+	arg(1, State, R).
 
 integrate(ave, Goal, Ave) :-
 	State = state(0, 0, _),
-	repeat,
-	(   call(Goal, V),
-	    arg(1, State, C), U is C+1, nb_setarg(1, State, U),
-	    arg(2, State, S), T is S+V, nb_setarg(2, State, T),
-	    fail
-	;   arg(1, State, C), arg(2, State, S), Ave is S/C
-	).
+	forall(call(Goal, V),
+	(	arg(1, State, C), U is C+1, nb_setarg(1, State, U),
+		arg(2, State, S), T is S+V, nb_setarg(2, State, T)
+	)),
+	arg(1, State, C), C > 0, arg(2, State, S), Ave is S/C.
 
 :- meta_predicate integrate(+, :, ?).
